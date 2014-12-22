8712d28ca0e13323666e901d828c899293936be8
Josh Souza <Josh.Souza@pearson.com>
2014-12-22 13:25:08 -0700

    Make the bootstrap run

--- modules/profiles/manifests/pureinsomnia_webserver.pp
+++ modules/profiles/manifests/pureinsomnia_webserver.pp
@@ -1,4 +1,5 @@
 class profiles::pureinsomnia_webserver () {
+  $puppet_home = '/opt/puppet_repo/pureinsomnia-puppet'
   file { '/var/www':
     ensure => 'directory',
   }
@@ -8,12 +9,17 @@ class profiles::pureinsomnia_webserver () {
     require => File['/var/www']
   }
   cron { 'puppet-run':
-    command => '/usr/bin/puppet apply /opt/puppet_repo/manifests/default.pp --modulepath="/opt/puppet_repo/modules:/etc/puppet/modules" --hiera_config "/opt/puppet_repo/hiera.yaml"',
+    command => "/usr/bin/puppet apply ${puppet_home}/manifests/default.pp --modulepath=\"${puppet_home}/modules:/etc/puppet/modules\" --hiera_config \"${puppet_home}/hiera.yaml\"",
     user => 'root',
     minute => '*/15',
   }
   cron { 'puppet-update':
     command => 'cd /opt/puppet_repo && /usr/bin/git pull',
+    user => 'root',
+    minute => '*/10',
+  }
+  cron { 'puppet-bootstrap':
+    command => "cd ${puppet_home} && ./bootstrap.sh",
     user => 'root',
     minute => '*/10',
   }
